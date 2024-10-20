00a2a09d105e5dc475ba8c8bcec9d4ac5e6b57d3
Yussuf Nergiz <yussufnergiz@Yussufs-MacBook-Pro.local>
2024-10-20 21:32:14 +0200

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { i18n } from "../../../i18n-config";
+
+export const runtime = "edge"
+
+function Header() {
+  const { locales, defaultLocale } = i18n;
+
+  return (
+    <header>
+      <div dir="ltr" className="languages">
+        {[...locales].sort().map((locale) => (
+          <Link
+            key={locale}
+            href={locale === defaultLocale ? "/" : `/${locale}`}
+          >
+            {locale}
+          </Link>
+        ))}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
